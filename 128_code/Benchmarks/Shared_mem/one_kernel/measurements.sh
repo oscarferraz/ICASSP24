9896cdea1d1820d5cc6e5bae9de907b8bce950c8
oscarferraz <oscar.ferraz@hotmail.com>
2023-09-01 18:16:55 +0100

    tx2 testing

--- 128_code/Benchmarks/Shared_mem/one_kernel/measurements.sh
+++ 128_code/Benchmarks/Shared_mem/one_kernel/measurements.sh
@@ -6,8 +6,13 @@ for i in {1..20}
 do
     sleep 1
    ./min_sum
-
+    
 done
+
+rm GPU_min_sum.txt
+rm memset.txt
+rm HtoD.txt
+rm DtoH.txt
 
 make
 
@@ -15,7 +20,8 @@ for i in {1..20}
 do
     sleep 1
     /usr/local/cuda-10.2/bin/nvprof --log-file profiler_GPU.log ./min_sum
-    grep -F "GPU_min_sum" profiler_GPU.log
-    grep -F "[CUDA memcpy HtoD]" profiler_GPU.log
-    grep -F "[CUDA memcpy DtoH]" profiler_GPU.log
+    grep -F "GPU_min_sum" profiler_GPU.log >> GPU_min_sum.txt
+    grep -F "[CUDA memset]" profiler_GPU.log >> memset.txt
+    grep -F "[CUDA memcpy HtoD]" profiler_GPU.log >> HtoD.txt
+    grep -F "[CUDA memcpy DtoH]" profiler_GPU.log >> DtoH.txt
 done
